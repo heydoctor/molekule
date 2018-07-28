78082f8219f36b522263849f1a0d1f5f0ee9c14b
Kyle Alwyn <kjalwyn@gmail.com>
2018-07-27 18:35:32 -0700

    BREAKING: Pull mdi icons from hosted css rather than mdi-react

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -1,15 +1,32 @@
 import React from 'react';
-import * as MdiIcon from 'mdi-react';
 import Proptypes from 'prop-types';
 
-const Icon = ({ name, ...props }) => {
-  const parsed = name
-    .split('-')
-    .map(l => l.charAt(0).toUpperCase() + l.slice(1))
-    .join('');
-  const IconName = MdiIcon[`${parsed}Icon`];
-  return <IconName {...props} />;
-};
+class Icon extends React.Component {
+  static injected = false;
+  static iconUrl = 'https://cdn.heydoctor.co/assets/css/material-design-icons.css';
+  static iconPrefix = 'mdi';
+
+  constructor(props) {
+    super(props);
+    if (!this.constructor.injected) {
+      const link = document.createElement('link');
+      link.rel = 'stylesheet';
+      link.type = 'text/css';
+      link.href = this.constructor.iconUrl;
+      document.head.appendChild(link);
+      this.constructor.injected = true;
+    }
+  }
+
+  render() {
+    const { name, size, color } = this.props;
+    const { iconPrefix } = this.constructor;
+
+    return (
+      <i className={`${iconPrefix} ${iconPrefix}-${name}`} style={{ fontSize: size, color: color || 'inherit' }} />
+    );
+  }
+}
 
 Icon.propTypes = {
   name: Proptypes.string.isRequired,
