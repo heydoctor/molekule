6398e439cc11c01dbcacf4dfd4b65b3a54f53a91
Kyle Alwyn <kjalwyn@gmail.com>
2018-09-17 17:21:05 -0700

    fix(Icon): Inject icon css on mount to fix SSR builds

--- src/Icon/Icon.js
+++ src/Icon/Icon.js
@@ -27,16 +27,16 @@ class Icon extends React.Component {
   static iconUrl = 'https://cdn.heydoctor.co/assets/css/material-design-icons.css';
   static iconPrefix = 'mdi';
 
-  constructor(props) {
+  componentDidMount(props) {
     super(props);
 
     if (!this.constructor.injected) {
+      this.constructor.injected = true;
       const link = document.createElement('link');
       link.rel = 'stylesheet';
       link.type = 'text/css';
       link.href = this.constructor.iconUrl;
       document.head.appendChild(link);
-      this.constructor.injected = true;
     }
   }
 
