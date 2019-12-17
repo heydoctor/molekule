316312516f40bc593e8694b5fc990374639b91eb
Kyle Alwyn <kjalwyn@gmail.com>
2019-12-17 10:17:00 -0800

    fix(Button): Use transparent background to override default white buttonface

--- src/theme.js
+++ src/theme.js
@@ -79,6 +79,7 @@ export default (customTheme = {}) => {
       },
     },
     secondary: {
+      backgroundColor: 'transparent',
       borderColor: colors.primary,
       color: colors.primary,
       '&:hover': {
